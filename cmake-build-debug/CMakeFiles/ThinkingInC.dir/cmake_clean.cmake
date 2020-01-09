57435eccb50e2bcecb009926e0eeea411e8efcd3
liuning11 <liuning11@jd.com>
2020-01-09 09:24:34 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/ThinkingInC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThinkingInC.dir/main.cpp.o"
+  "ThinkingInC"
+  "ThinkingInC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThinkingInC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
